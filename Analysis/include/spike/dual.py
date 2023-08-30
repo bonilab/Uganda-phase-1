859040e92f725b8cbb56e709f42624b0a3fe48b1
rjzupkoii <rjzii@yahoo.com>
2023-08-30 13:44:49 -0400

    Move class to its own file

--- Analysis/include/spike/dual.py
+++ Analysis/include/spike/dual.py
@@ -1,6 +1,6 @@
-# district.py
+# dual.py
 #
-# Include file for the spiking.py script that defines the district plotting class.
+# Include file for the spiking.py script that dual spike plotting class.
 import datetime
 import matplotlib
 import matplotlib.pyplot as plt
@@ -8,13 +8,31 @@ import pandas as pd
 
 import include.spike.common as shared
 
-# This class wraps the functions related to plotting district spike studies
-class district:
+# This class wraps the functions related to plotting dual spike studies and 
+# the spike calibration / validation studies.
+class dual_spike:
+  # Note the assumed model year
+  MODEL_YEAR = 2004
+
   mutations = None
   labels = None
   
-  def __plot(self, replicates, year, ylabel, title, filename):
-    DATES, DISTRICT, INFECTED, WEIGHTED = 2, 3, 4, 8
+  def __plot(self, replicates, mutation, ylabel, title, filename):
+    DATES, DISTRICT, INFECTIONS = 2, 3, 4
+    MAPPING = { '469Y' : 8, '675V' : 11, 'either' : 14 }
+
+    def add_points():
+      row, col = 0, 0
+      for district in districts:
+        plt.sca(axes[row, col])
+        for index, data_row in self.mutations[self.mutations.District == district].iterrows():
+          x = datetime.datetime(data_row.Year, 9, 30)
+          y = data_row.Frequency
+          plt.scatter(x, y, color = 'black', s = 100, zorder = 99)
+        row, col = shared.increment(row, col)
+
+    # Note the correct index of the weighted values
+    weighted = MAPPING[mutation]
   
     # Setup to generate the plot
     matplotlib.rc_file('../Scripts/matplotlibrc-line')
@@ -29,13 +47,12 @@ class district:
     for replicate in replicates:
       # Load the data and prepare the dates
       data = pd.read_csv('data/spiking/{}.csv'.format(replicate), header = None)
-      data['frequency'] = data[WEIGHTED] / data[INFECTED]
+      data['frequency'] = data[weighted] / data[INFECTIONS]
       ymax = max(ymax, max(data.frequency))
       dates = data[DATES].unique().tolist()
-      dates = [datetime.datetime(year, 1, 1) + datetime.timedelta(days=x) for x in dates]
+      dates = [datetime.datetime(self.MODEL_YEAR, 1, 1) + datetime.timedelta(days=x) for x in dates]
   
-      # Generate a 15 panel plot while looping over the districts that we have 
-      # spiking data for
+      # Generate a 15 panel plot while looping over the districts that we have spiking data for
       row, col = 0, 0
       for district in districts:
         district_id = self.labels[self.labels.Label == district].ID.values[0]
@@ -43,15 +60,8 @@ class district:
         axes[row, col].title.set_text(district)
         row, col = shared.increment(row, col)
           
-    # Next, add the know data points to the plots
-    row, col = 0, 0
-    for district in districts:
-      plt.sca(axes[row, col])
-      for index, data_row in self.mutations[self.mutations.District == district].iterrows():
-        x = datetime.datetime(data_row.Year, 9, 30)
-        y = data_row.Frequency
-        plt.scatter(x, y, color = 'black', s = 100, zorder = 99)
-      row, col = shared.increment(row, col)
+    # Next, add the known data points to the plots unless we are plotting the total resistance
+    if mutation != 'either': add_points()
           
     # Format the x, y axis and ticks
     for row in range(3):
@@ -77,58 +87,44 @@ class district:
     plt.savefig('plots/{}'.format(filename))
     plt.close()
   
-  def process(self, mutation):
+  def __process(self, mutation):
     CONFIGURATION, REPLICATE, FILENAME = 0, 3, 2
-  
+
     # Load relevant data
     data = pd.read_csv(shared.REPLICATES_LIST, header = None)
     self.labels = pd.read_csv(shared.DISTRICTS_MAPPING)
-    self.mutations = pd.read_csv(shared.MUTATIONS_TEMPLATE.format(mutation))
+    if mutation == 'either':
+      self.mutations = pd.read_csv(shared.MUTATIONS_TEMPLATE.format('675V'))
+    else:  
+      self.mutations = pd.read_csv(shared.MUTATIONS_TEMPLATE.format(mutation))
   
     configurations = []
     shared.progressBar(0, len(data))
     for index, row in data.iterrows():
       try:
-        # Skip if this is not a district calibration
+        # Check to see if we can skip this entry
         if len(data[data[FILENAME] == row[FILENAME]]) == 1: continue
-        
-        # Skip if we have already seen this configuration
         if row[CONFIGURATION] in configurations: continue
 
-        # Get the list of replicates associated with this configuration, note that
-        # we are assuming that using the configuration id is more reliable to distinguish
-        # between configurations than their filename
+        # Get the list of replicates associated with this configuration
         replicates = data[data[CONFIGURATION] == row[CONFIGURATION]][REPLICATE]
 
-        # Determine the year of the study, initially studies started in 2009,
-        # but as calibration progressed it was changed and added to the YAML filename
-        version = None
-        parts = row[FILENAME].replace('.yml', '').split('-')
-        if len(parts) == 4:
-          year, spike, population = 2009, 0.075, float(parts[3])
-        elif len(parts) == 6:
-          year, spike = int(parts[3]), float(parts[4])
-          if 'v' in parts[5]:
-            population = float(parts[5][:parts[5].index('v') - 1])
-            version = parts[5][parts[5].index('v') + 1:]
-          else:
-            population = float(parts[5])
-        else:
-          exit('\nUnrecognized file format: {}'.format(row[FILENAME]))
-
-        # Prepare the filename and title
-        title = '{} (spike: {:.1f}%, pop.: {}%)'.format(parts[2].capitalize(), spike * 100.0, int(population * 100.0))
-        filename = '{}/uga-{}-{}-{}-{}.png'.format(mutation, parts[2], year, spike, population)
-        if version is not None:
-          title += ', version {}'.format(version)
-          filename = '{}/uga-{}-{}-{}-{}-v{}.png'.format(
-            mutation, parts[2], year, spike, population, version)
+        # Set the title and filename for the results
+        title = 'Spike Calibration, {}'.format(mutation)
+        if mutation == 'either':
+          title = 'Total ART Resistance'
+        filename = 'uga-spike-{}-{}.png'.format(row[CONFIGURATION], mutation)
 
         # Prepare the plot, note the configuration
-        self.__plot(replicates, year, '{} Frequency'.format(mutation), title, filename)
+        self.__plot(replicates, mutation, '{} Frequency'.format(mutation), title, filename)
         configurations.append(row[CONFIGURATION])
         shared.progressBar(index, len(data))
       except Exception as ex:
           print('\nError plotting replicate {}, configuration {}'.format(row[REPLICATE], row[FILENAME]))
           print(ex)
     shared.progressBar(len(data), len(data))    
+
+  def process(self):
+    self.__process('469Y')
+    self.__process('675V')
+    self.__process('either')
