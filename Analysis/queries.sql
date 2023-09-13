261507b74fd5d5915b541f4f28d17329ad7ed7a0
rjzupkoii <rjzii@yahoo.com>
2023-09-13 17:51:21 -0400

    Update query

--- Analysis/queries.sql
+++ Analysis/queries.sql
@@ -10,7 +10,10 @@ group by c.id, filename, replicateid, starttime
 order by modeldays desc
 
 -- Query for IRS data
-SELECT a.*, b.occurrences, b.clinicaloccurrences, b.weightedoccurrences
+SELECT a.*, 
+	CASE WHEN b.occurrences IS NULL THEN 0 ELSE b.occurrences END as occurances,
+	CASE WHEN b.clinicaloccurrences IS NULL THEN 0 ELSE b.clinicaloccurrences END as clinicaloccurrences,
+	CASE WHEN b.weightedoccurrences IS NULL THEN 0 ELSE b.weightedoccurrences END as weightedoccurrences
 FROM (
   SELECT c.filename, r.id, md.dayselapsed, 
 	msd.infectedindividuals, msd.clinicalepisodes, msd.treatments, msd.treatmentfailures  
@@ -18,9 +21,9 @@ FROM (
 	INNER JOIN sim.replicate r ON r.configurationid = c.id
 	INNER JOIN sim.monthlydata md ON md.replicateid = r.id
 	INNER JOIN sim.monthlysitedata msd ON msd.monthlydataid = md.id
-  WHERE c.studyid = 4
+  WHERE c.filename ~ 'uga-irs-lamwo-*'
 	AND r.endtime IS NOT NULL) a
-INNER JOIN (
+LEFT JOIN (
   SELECT r.id, md.dayselapsed,
     CAST(sum(mgd.occurrences) AS INTEGER) AS occurrences,
 	CAST(sum(mgd.clinicaloccurrences) AS INTEGER) AS clinicaloccurrences,
@@ -30,7 +33,9 @@ INNER JOIN (
 	INNER JOIN sim.monthlydata md ON md.replicateid = r.id
 	INNER JOIN sim.monthlygenomedata mgd ON mgd.monthlydataid = md.id
 	INNER JOIN sim.genotype g ON g.id = mgd.genomeid
-  WHERE c.studyid = 4
+  WHERE c.filename ~ 'uga-irs-lamwo-*'
     AND r.endtime IS NOT NULL
 	AND g.name ~ '^.....Y..'
   GROUP BY r.id, md.dayselapsed) b ON b.id = a.id AND b.dayselapsed = a.dayselapsed
+WHERE filename NOT IN ('uga-irs-lamwo.yml', 'uga-irs-lamwo-steady.yml', 'uga-irs-lamwo-steady-zero.yml')
+ORDER BY dayselapsed
