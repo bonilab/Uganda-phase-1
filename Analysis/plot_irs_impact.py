f84d1d062265800846aaa308cc60b025963979e9
rjzupkoii <rjzii@yahoo.com>
2023-09-13 17:09:50 -0400

    Update IRS spraying plot

--- Analysis/plot_irs_impact.py
+++ Analysis/plot_irs_impact.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 # plot_irs_impact.py
 #
 # Generate impact plots for the IRS experiments.
+import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,12 +13,76 @@ import sys
 sys.path.insert(1, '../../PSU-CIDD-MaSim-Support/Python/include')
 from plotting import scale_luminosity
 
-
-def plot(filterOn):
-  FILENAME = 'data/irs-data.csv'
-  TEMPLATE = 'uga-irs-{}{}.yml'
+DATA = 'data/uga-lamwo.csv'
+MODEL_YEAR = 2004    
+    
+def plot_clinical():
+  # Load the data, note the unique replicates, dates, and filenames
+  data = pd.read_csv(DATA)
+  dates = data.dayselapsed.unique()
+  dates = [datetime.datetime(MODEL_YEAR, 1, 1) + datetime.timedelta(days=int(x)) for x in dates]
+  filenames = data.filename.unique()
   
-  def __plot(filename, ndx):
+  for filename in filenames:
+    
+    # Prepare the plot
+    matplotlib.rc_file('../Scripts/matplotlibrc-line')
+    replicates = []
+    for replicate in data[data.filename == filename].id.unique():
+      if len(replicates) == 0:
+        replicates = data[data.id == replicate].clinicalepisodes
+      else:
+        replicates = np.vstack((replicates, data[data.id == replicate].clinicalepisodes))
+        
+    # Calculate the median and IRQ
+    upper = np.percentile(replicates, 25, axis=0)
+    median = np.percentile(replicates, 50, axis=0)
+    lower = np.percentile(replicates, 75, axis=0)
+      
+    # Generate the plot
+    lines = plt.plot(dates, median)
+    color = scale_luminosity(lines[-1].get_color(), 1)
+    plt.fill_between(dates, lower, upper, alpha=0.5, facecolor=color)
+
+  # Format the plot    
+  plt.legend(filenames)
+  plt.ylim([0, 35000])
+  plt.xlim([min(dates), max(dates)])
+  plt.title('469Y Spike Comparison')
+  plt.ylabel('Clinical Cases (Shaded: IQR)')
+  plt.xlabel('Model Date')
+  
+  # Draw the intervention lines
+  plt.axvline(x=datetime.datetime(2005, 4, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2005, 5, 1), 30000, 'Spike 469Y', rotation=90, fontsize='small', color='gray')
+  plt.axvline(x=datetime.datetime(2014, 1, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2013, 10, 1), 24500, 'End of Routine Spraying', rotation=90, fontsize='small', color='gray')
+  plt.axvline(x=datetime.datetime(2017, 1, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2016, 10, 1), 27000, 'One Time Spraying', rotation=90, fontsize='small', color='gray')
+  plt.axvline(x=datetime.datetime(2018, 1, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2017, 10, 1), 28000, 'End of Spraying', rotation=90, fontsize='small', color='gray')
+  
+  # Save the figure
+  plt.savefig('plots/irs_clinical.png')
+  plt.close()
+
+
+def plot_irs():
+  # Load the data and calculate the frequency
+  data = pd.read_csv(DATA)
+  data['frequency'] = data.weightedoccurrences / data.infectedindividuals
+
+  # Note the unique replicates and dates
+  dates = data.dayselapsed.unique()
+  dates = [datetime.datetime(MODEL_YEAR, 1, 1) + datetime.timedelta(days=int(x)) for x in dates]
+  
+  # Note the unique filenaames
+  filenames = data.filename.unique()
+  
+  for filename in filenames:
+    
+    # Prepare the plot
+    matplotlib.rc_file('../Scripts/matplotlibrc-line')
     replicates = []
     for replicate in data[data.filename == filename].id.unique():
       if len(replicates) == 0:
@@ -23,47 +90,40 @@ def plot(filterOn):
       else:
         replicates = np.vstack((replicates, data[data.id == replicate].frequency))
         
-    # Calcluate the median and IRQ
-    upper = np.percentile(replicates, 75, axis=0)
+    # Calculate the median and IRQ
+    upper = np.percentile(replicates, 45, axis=0)
     median = np.percentile(replicates, 50, axis=0)
-    lower = np.percentile(replicates, 25, axis=0)
-    
+    lower = np.percentile(replicates, 55, axis=0)
+      
     # Generate the plot
-    axes[ndx].plot(dates, median)
-    color = scale_luminosity(axes[ndx].lines[-1].get_color(), 1)
-    axes[ndx].fill_between(dates, lower, upper, alpha=0.5, facecolor=color)
+    lines = plt.plot(dates, median)
+    color = scale_luminosity(lines[-1].get_color(), 1)
+    plt.fill_between(dates, lower, upper, alpha=0.5, facecolor=color)
+
+  # Format the plot    
+  plt.legend(filenames)
+  plt.ylim([0, 1])
+  plt.xlim([min(dates), max(dates)])
+  plt.title('469Y Spike Comparison')
+  plt.ylabel('469Y frequency (Median, Shaded: ±5%)')
+  plt.xlabel('Model Date')
   
-  # Load the data and calculate the frequency
-  data = pd.read_csv(FILENAME)
-  data['frequency'] = data.weightedoccurrences / data.infectedindividuals
+  # Draw the intervention lines
+  plt.axvline(x=datetime.datetime(2005, 4, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2005, 5, 1), 0.87, 'Spike 469Y', rotation=90, fontsize='small', color='gray')
+  plt.axvline(x=datetime.datetime(2014, 1, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2014, 2, 1), 0.7, 'End of Routine Spraying', rotation=90, fontsize='small', color='gray')
+  plt.axvline(x=datetime.datetime(2017, 1, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2017, 2, 1), 0.78, 'One Time Spraying', rotation=90, fontsize='small', color='gray')
+  plt.axvline(x=datetime.datetime(2018, 1, 1), color='gray', ls='--')
+  plt.text(datetime.datetime(2018, 2, 1), 0.81, 'End of Spraying', rotation=90, fontsize='small', color='gray')
   
-  # Note the unique replicates and dates
-  dates = data.dayselapsed.unique()
+  # Save the figure
+  plt.savefig('plots/irs_frequency.png')
+  plt.close()
   
-  # Prepare the plot
-  matplotlib.rc_file('../Scripts/matplotlibrc-line')
-  figure, axes = plt.subplots(2, 1)
-  __plot(TEMPLATE.format(filterOn, ''), 0) 
-  __plot(TEMPLATE.format(filterOn, '-steady'), 1)
-    
-  # Draw the intervention line, 2015
-  axes[0].axvline(x=4018, color='green')
-  
-  # Draw beta intervention lines
-  axes[0].axvline(x=4749, color='red')                          # 2017
-  axes[0].axvline(x=5114, color='red')                          # 2018
-  if filterOn == 'high': axes[0].axvline(x=5479, color='red')   # 2019
-  
-  # Format the plot
-  axes[0].title.set_text('{} Prevalence, IRS'.format(filterOn.capitalize()))
-  axes[1].title.set_text('{} Prevalence, No Intervention'.format(filterOn.capitalize()))
-  axes[1].set_xlabel('Model Days Elapsed')
-  for ndx in [0, 1]:
-    axes[ndx].set_ylim([0, 1])
-    axes[ndx].set_xlim([min(dates), max(dates)])
-    axes[ndx].set_ylabel('469Y frequency')
-        
     
 if __name__ == '__main__':
-  plot('high')
-  # plot('low')
+  plot_clinical()
+  plot_irs()
+  
