6c1c3c0be5962496bec7d573192a1f2b90b3884b
rjzupkoii <rjzii@yahoo.com>
2023-09-29 15:18:38 -0400

    Updated monitoring queries

--- Analysis/queries.sql
+++ Analysis/queries.sql
@@ -1,13 +1,22 @@
 -- Basic monitoring query
-select c.id as configurationid, c.studyid, filename, replicateid, 
-  starttime, now() - starttime as runningtime, 
-  max(dayselapsed) as modeldays
+select c.id as configurationid, replicateid, filename,
+  to_date('2004-01-01', 'YYYY-MM-DD') + max(dayselapsed) as modeldata, max(dayselapsed) as dayselapsed,
+  starttime, now() - starttime as runningtime
 from sim.replicate r
   inner join sim.configuration c on c.id = r.configurationid
   inner join sim.monthlydata md on md.replicateid = r.id
 where r.endtime is null
-group by c.id, filename, replicateid, starttime
-order by modeldays desc
+group by c.id, replicateid, filename, starttime
+order by dayselapsed desc
+
+-- Query for the replicate count
+select c.filename, count(r.id) as replicates, 
+  sum(case when r.endtime is not null then 1 else 0 end) as complete
+from sim.configuration c
+  inner join sim.replicate r on c.id = r.configurationid
+where c.studyid = 5
+group by c.filename
+order by c.filename
 
 -- Query for IRS data
 SELECT a.*, 
